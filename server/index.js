de75c0103e90491b40a7d6e631f502c78641a4ef
Bailey <baileyrosefern@gmail.com>
2023-01-12 16:05:05 -0700

    able to render, used something that was not deprecated

--- server/index.js
+++ server/index.js
@@ -18,6 +18,9 @@ app.use(cors());
 const CONNECTION_URL = 'mongodb+srv://BaileyFern:testing123@sei.tz0cct9.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
+mongoose.set('strictQuery', true);
+
+
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
     .catch((error)=> console.log(error.message));
