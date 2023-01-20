4fcc79a7d388c4ca3f8b0205b53bef775208c79a
Bailey <baileyrosefern@gmail.com>
2023-01-20 11:09:57 -0700

    Saving again

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+//Backend url data
+
+const url="https://backend-memory-app.onrender.com";
+
+// const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
